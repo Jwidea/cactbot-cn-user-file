5573f7b813eb8ca5df616dfee1e8430fb4b9dcc0
Jingwei <you@example.com>
2020-10-23 16:24:35 -0500

    update test use arr trails

--- raidboss.js
+++ raidboss.js
@@ -1,6 +1,11 @@
 'use strict';
 import * as Shb from './script/05_shb/index'
+import * as Arr from './script/02_arr/index'
 
 for (const k in Shb) {
   Options.Triggers.push(Shb[k].script)
 }
+
+for (const k in Arr ) {
+  Options.Triggers.push(Arr[k].script)
+} 
